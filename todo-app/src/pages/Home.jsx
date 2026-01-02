d2b00bd579a1c5fbbbbc755c4289df06708521a5
Ayush Kumar <ayush.kumar@w3villa.com>
2026-01-02 18:20:58 +0530

    Added register button

--- todo-app/src/pages/Home.jsx
+++ todo-app/src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const handleNavigation = () => {
+    navigate("/registration");
+  };
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>For registration </p>
+      <button onClick={handleNavigation}>Register</button>
+    </div>
+  );
 };
 
 export default Home;
